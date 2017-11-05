4927ae01e98f3c388c05bfa7ca539359b3b381e6
Duncan Grant <dalaidunc@gmail.com>
2017-11-05 23:17:27 +0000

    add arrowheads to links

--- poc/app.js
+++ poc/app.js
@@ -115,7 +115,26 @@ function restart({ nodes, links }) {
   // Apply the general update pattern to the links.
   link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
   link.exit().remove();
-  link = link.enter().append("line").merge(link);
+  link = link.enter().append("line")
+    .attr('class', 'link')
+    .attr('marker-end', 'url(#end)')
+    .merge(link);
+
+  // create the arrowheads for links
+  g.append('defs').selectAll('marker')
+    .exit()
+    .data(['end'])
+    .enter()
+    .append('svg:marker')
+    .attr('id', String)
+    .attr('viewBox', '0 -5 10 10')
+    .attr('refX', 15)
+    .attr('refY', -1.5)
+    .attr('markerWidth', 12)
+    .attr('markerHeight', 12)
+    .attr('orient', 'auto')
+    .append('svg:path')
+    .attr('d', 'M0,-5L10,0L0,5');
 
   // Update and restart the simulation.
   simulation.nodes(nodes);
@@ -127,8 +146,37 @@ function restart({ nodes, links }) {
 function ticked() {
   node.attr('transform', d => `translate(${d.x}, ${d.y})`);
 
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+  // as the node size grows currently the arrowhead needs to change the marker width/height to match
+  // TODO: how to set marker at right place given varying sized nodes?
+  link
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; })
+/*
+ *     .attr("x2", function(d) { 
+ *       var diffX = d.target.x - d.source.x;
+ *       var diffY = d.target.y - d.source.y;
+ * 
+ *       // Length of path from center of source node to center of target node
+ *       var pathLength = Math.sqrt((diffX * diffX) + (diffY * diffY));
+ * 
+ *       // x and y distances from center to outside edge of target node
+ *       var offsetX = (diffX * 8) / pathLength;
+ *       var offsetY = (diffY * 8) / pathLength;
+ *       return d.target.x - offsetX; 
+ *     })
+ *     .attr("y2", function(d) { 
+ *       var diffX = d.target.x - d.source.x;
+ *       var diffY = d.target.y - d.source.y;
+ * 
+ *       // Length of path from center of source node to center of target node
+ *       var pathLength = Math.sqrt((diffX * diffX) + (diffY * diffY));
+ * 
+ *       // x and y distances from center to outside edge of target node
+ *       var offsetX = (diffX * 8) / pathLength;
+ *       var offsetY = (diffY * 8) / pathLength;
+ *       return d.target.y - offsetY; 
+ *     });
+ */
 }
